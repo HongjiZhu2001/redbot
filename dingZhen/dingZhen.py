a939bc059c33e78319cd58ed6d5f599a35d678c8
HongjiZhu2001 <73827095+HongjiZhu2001@users.noreply.github.com>
2022-04-25 17:12:46 -0700

    Update dingZhen.py

--- dingZhen/dingZhen.py
+++ dingZhen/dingZhen.py
@@ -15,24 +15,9 @@ __version__ = "2018.9.0"
 __author__ = "Yukirin"
 
 gifs = [
-    "https://i.imgur.com/YTGnx49.gif",
-    "https://i.imgur.com/U37wHs9.gif",
-    "https://i.imgur.com/BU2IQym.gif",
-    "https://i.imgur.com/yp6kqPI.gif",
-    "https://i.imgur.com/uDyehIe.gif",
-    "https://i.imgur.com/vG8Vuqp.gif",
-    "https://i.imgur.com/z4uCLUt.gif",
-    "https://i.imgur.com/ZIRC9f0.gif",
-    "https://i.imgur.com/s8m4srp.gif",
-    "https://i.imgur.com/LKvNxmo.gif",
-    "https://i.imgur.com/j4W4GFt.gif",
-    "https://i.imgur.com/75bX4A1.gif",
-    "https://i.imgur.com/dSlfpe3.gif",
-    "https://i.imgur.com/JjxaT8e.gif",
-    "https://i.imgur.com/QWBlOaQ.gif",
-    "https://i.imgur.com/5448px6.gif",
-    "https://i.imgur.com/4WJRAGw.gif",
-    "https://i.imgur.com/v1sSh5r.gif"
+    "https://i.imgur.com/TZjlBlw.jpg",
+    "https://i.imgur.com/q4L8PCS.jpg",
+    "https://i.imgur.com/mcsQg4I.jpg",
 ]
 
 failmsgs = [
@@ -45,9 +30,7 @@ failmsgs = [
 ]
 
 patmsgs = [
-    "**{user}** got a pat from **{author}**",
-    "**{author}** affectionately pat **{user}**",
-    "Without hesitation, **{author}** pats **{user}** with love"
+    ""
 ]
 
 
@@ -62,21 +45,17 @@ class PDA(BaseCog):
 
     @commands.command()
     @commands.cooldown(6, 60, commands.BucketType.user)
-    async def pat(self, ctx, *, user: discord.Member=None):
+    async def dingZhen(self, ctx, *, user: discord.Member=None):
         """Pat users."""
         author = ctx.author
 
-        if not user:
-            message = rnd(self.failmsgs)
-            await ctx.send(message.format(author=author.name))
-        else:
             message = rnd(patmsgs)
             pat = discord.Embed(description=message.format(user=user.name, author=author.name), color=discord.Color(0xffb6c1))
             pat.set_image(url=rnd(self.gifs))
             await ctx.send(embed=pat)
 
     @commands.command(name="pdaver", hidden=True)
-    async def _pda_version(self, ctx):
+    async def _dingZhen_version(self, ctx):
         """Show PDA version"""
         ver = self.version
         await ctx.send("You are using PDA version {}".format(ver))
