a1a774d75fe86f878d1f4085d295063db752865f
HongjiZhu2001 <73827095+HongjiZhu2001@users.noreply.github.com>
2022-04-25 18:24:41 -0700

    Update faBing.py

--- faBing/faBing.py
+++ faBing/faBing.py
@@ -16,23 +16,6 @@ __author__ = "Yukirin"
 
 gifs = [
     "https://i.imgur.com/YTGnx49.gif",
-    "https://i.imgur.com/U37wHs9.gif",
-    "https://i.imgur.com/BU2IQym.gif",
-    "https://i.imgur.com/yp6kqPI.gif",
-    "https://i.imgur.com/uDyehIe.gif",
-    "https://i.imgur.com/vG8Vuqp.gif",
-    "https://i.imgur.com/z4uCLUt.gif",
-    "https://i.imgur.com/ZIRC9f0.gif",
-    "https://i.imgur.com/s8m4srp.gif",
-    "https://i.imgur.com/LKvNxmo.gif",
-    "https://i.imgur.com/j4W4GFt.gif",
-    "https://i.imgur.com/75bX4A1.gif",
-    "https://i.imgur.com/dSlfpe3.gif",
-    "https://i.imgur.com/JjxaT8e.gif",
-    "https://i.imgur.com/QWBlOaQ.gif",
-    "https://i.imgur.com/5448px6.gif",
-    "https://i.imgur.com/4WJRAGw.gif",
-    "https://i.imgur.com/v1sSh5r.gif"
 ]
 
 failmsgs = [
@@ -102,7 +85,7 @@ class FaBing(BaseCog):
             fb = discord.Embed(description=message.format(user=user.name, author=author.name), color=discord.Color(0xffb6c1))
             await ctx.send(embed=fb)
 
-    @commands.command(name="pdaver", hidden=True)
+    @commands.command(name="fbver", hidden=True)
     async def _pda_version(self, ctx):
         """Show PDA version"""
         ver = self.version
