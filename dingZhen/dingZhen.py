b72c553d875f70083baf70f0903ec42f4f00f78b
HongjiZhu2001 <73827095+HongjiZhu2001@users.noreply.github.com>
2022-04-25 17:33:31 -0700

    Update and rename pda.py to dingZhen.py

--- dingZhen/dingZhen.py
+++ dingZhen/dingZhen.py
@@ -15,24 +15,9 @@ __version__ = "2018.9.0"
 __author__ = "Yukirin"
 
 gifs = [
-    "https://i.imgur.com/YTGnx49.gif",
-    "https://i.imgur.com/U37wHs9.gif",
-    "https://i.imgur.com/BU2IQym.gif",
-    "https://i.imgur.com/yp6kqPI.gif",
-    "https://i.imgur.com/uDyehIe.gif",
-    "https://i.imgur.com/vG8Vuqp.gif",
-    "https://i.imgur.com/z4uCLUt.gif",
-    "https://i.imgur.com/ZIRC9f0.gif",
-    "https://i.imgur.com/s8m4srp.gif",
-    "https://i.imgur.com/LKvNxmo.gif",
-    "https://i.imgur.com/j4W4GFt.gif",
-    "https://i.imgur.com/75bX4A1.gif",
-    "https://i.imgur.com/dSlfpe3.gif",
-    "https://i.imgur.com/JjxaT8e.gif",
-    "https://i.imgur.com/QWBlOaQ.gif",
-    "https://i.imgur.com/5448px6.gif",
-    "https://i.imgur.com/4WJRAGw.gif",
-    "https://i.imgur.com/v1sSh5r.gif"
+    "https://i.imgur.com/TZjlBlw.jpg",
+    "https://i.imgur.com/mcsQg4I.jpg",
+    "https://i.imgur.com/UkSYd6R.jpg"
 ]
 
 failmsgs = [
